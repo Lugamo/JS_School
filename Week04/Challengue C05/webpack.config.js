0192f3f4a18332b3f416192b344f34c5378590a4
lugamo <bitlugamo@gmail.com>
2018-11-10 17:23:58 -0500

    Adding instruccions to the readme

--- Week04/Challengue C05/webpack.config.js
+++ Week04/Challengue C05/webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
       },
       {
         test: /\.m?js$/,
