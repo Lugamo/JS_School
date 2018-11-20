e0ea00d270d47a1d48a21046f810146f136b42be
lugamo <bitlugamo@gmail.com>
2018-11-20 14:50:57 -0500

    Base Project

---
+++ Week06/Challenge C07/src/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import './styles/style.scss';
+import './side-menu';
+
+// Request to get the data from ther server
+/*
+const ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'http://localhost:3000/books', true);
+ourRequest.withCredentials = true;
+ourRequest.onload = () => {
+  if (ourRequest.status >= 200 && ourRequest.status < 400) {
+    const data = JSON.parse(ourRequest.responseText);
+    console.log(data);
+  } else {
+    console.log('We connected to the server, but it returned an error.');
+  }
+};
+ourRequest.onerror = () => {
+  console.log('Connection error');
+};
+ourRequest.send();
+*/
---
+++ Week06/Challenge C07/webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: `${__dirname}/dist`,
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+      {
+        test: /\.otf$/,
+        use: [{
+          loader: 'file-loader',
+        }],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 5000,
+          },
+        }],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+};
