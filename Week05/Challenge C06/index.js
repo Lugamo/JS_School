e66ee4c6cc0dce59ffc3b350c71ef6cc67a50bc5
lugamo <bitlugamo@gmail.com>
2018-12-04 10:50:25 -0500

    Change the default port from 3000 to 3001

--- Week05/Challenge C06/index.js
+++ Week05/Challenge C06/index.js
@@ -6,7 +6,7 @@ const bookController = require('./controllers/bookController');
 const credentials = require('./controllers/credentialsController');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
